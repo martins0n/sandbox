5be5d8f92cef18860c9cc101450a703ff94854b3
martins0n <martinmail@mail.ru>
2020-03-02 18:48:11 +0300

    FIX: indexing

--- seer/m4forecaster.py
+++ seer/m4forecaster.py
@@ -16,99 +16,105 @@ def pd_series_to_ts(ts: pd.Series):
     
     """
     freq = ts.index.freq
-    rstats = importr('stats')
-    
-    ts = ts.astype('float')
+    rstats = importr("stats")
+
+    ts = ts.astype("float")
     start_year = ts.index[0].year
     start_month = ts.index[0].month
     end_year = ts.index[-1].year
     end_month = ts.index[-1].month
 
-    if ts.index.freq == 'M':
+    if ts.index.freq == "M":
         return rstats.ts(
             FloatVector(ts.values),
             start=IntVector((start_year, start_month)),
-            end=IntVector((end_year, end_month)), frequency=12
+            end=IntVector((end_year, end_month)),
+            frequency=12,
         )
-    
-    elif ts.index.freq == 'Y':
+
+    elif ts.index.freq == "Y":
         return rstats.ts(
             FloatVector(ts.values),
             start=IntVector((start_year,)),
-            end=IntVector((end_year,)), 
-            frequency=1
+            end=IntVector((end_year,)),
+            frequency=1,
         )
-    
-    elif ts.index.freq == 'Q':
+
+    elif ts.index.freq == "Q":
         return rstats.ts(
             FloatVector(ts.values),
             start=IntVector((start_year, start_month)),
-            end=IntVector((end_year, end_month)), frequency=4
+            end=IntVector((end_year, end_month)),
+            frequency=4,
         )
 
-    elif ts.index.freq == 'W':
+    elif ts.index.freq == "W":
         start_index = (
-            (ts.index[0] - pd.to_datetime('1970-01-01'))
-            .total_seconds() / 3600 / 24 // 7
+            (ts.index[0] - pd.to_datetime("1970-01-01")).total_seconds()
+            / 3600
+            / 24
+            // 7
         )
         end_index = (
-            (ts.index[-1] - pd.to_datetime('1970-01-01'))
-            .total_seconds() / 3600 / 24 // 7
+            (ts.index[-1] - pd.to_datetime("1970-01-01")).total_seconds()
+            / 3600
+            / 24
+            // 7
         )
         return rstats.ts(
             FloatVector(ts.values),
             start=IntVector((start_index)),
-            end=IntVector((end_index)), frequency=1
+            end=IntVector((end_index)),
+            frequency=1,
         )
-    
-    elif ts.index.freq == 'H':
+
+    elif ts.index.freq == "H":
         start_index = (
-            (ts.index[0] - pd.to_datetime('1970-01-01'))
-            .total_seconds() // 3600
-        )
+            ts.index[0] - pd.to_datetime("1970-01-01")
+        ).total_seconds() // 3600
         end_index = (
-            (ts.index[-1] - pd.to_datetime('1970-01-01'))
-            .total_seconds() // 3600
+            ts.index[-1] - pd.to_datetime("1970-01-01")
+        ).total_seconds() // 3600
+        return rstats.ts(
+            FloatVector(ts.values),
+            start=IntVector((start_index,)),
+            end=IntVector((end_index,)),
+            frequency=1,
+        )
+
+    elif ts.index.freq == "D":
+        start_index = (ts.index[0] - pd.to_datetime("1970-01-01")).total_seconds() // (
+            3600 * 24
+        )
+        end_index = (ts.index[-1] - pd.to_datetime("1970-01-01")).total_seconds() // (
+            3600 * 24
         )
         return rstats.ts(
             FloatVector(ts.values),
             start=IntVector((start_index,)),
-            end=IntVector((end_index,)), frequency=1
-        )
-    
-    elif ts.index.freq == 'D':
-        start_index = (
-            (ts.index[0] - pd.to_datetime('1970-01-01'))
-            .total_seconds() // (3600 * 24)
-        )
-        end_index = (
-            (ts.index[-1] - pd.to_datetime('1970-01-01'))
-            .total_seconds() // (3600 * 24)
-        )
-        return rstats.ts(
-            FloatVector(ts.values),
-            start=IntVector((start_index,)),
-            end=IntVector((end_index,)), frequency=1
+            end=IntVector((end_index,)),
+            frequency=1,
         )
     else:
         raise NotImplementedError
-    
 
+
+"""
 def forecast(ts: pd.Series):
     ts = pd_series_to_ts(ts)
     _forecast = importr('forecast')
     return _forecast.Arima(ts, order=IntVector((2,0,0)))
+"""
 
 
-def m4meta_train(mode_path):
+def m4meta_train(model_path):
     r_string = """
     train_model_func <- function(name){
         library(M4metalearning)
         library(M4comp2018)
         library(tsfeatures)
         set.seed(31-05-2018)
-        indices <- sample(length(M4))
-        M4_train <- M4[ indices[1:15]]
+        M4_train <- M4
         M4_train <- temp_holdout(M4_train)
         M4_train <- calc_forecasts(M4_train, forec_methods(), n.cores=4)
         M4_train <- calc_errors(M4_train)
@@ -119,20 +125,19 @@ def m4meta_train(mode_path):
     }
     """
     rfunc = r(r_string)
-    rfunc(mode_path)
+    rfunc(model_path)
 
 
-def load_model(str_name='temp.Rdata'):
-    r['load'](str_name)
-    model = r['meta_model']
+def load_model(str_name="temp.Rdata"):
+    r["load"](str_name)
+    model = r["meta_model"]
     return model
 
 
 def make_prediction(ts: pd.Series, model_path, h=6, full=False):
-    
-    
+
     meta_model = load_model(model_path)
-    
+
     ts_r = pd_series_to_ts(ts)
 
     ts_freq = ts.index[-1] - ts.index[-2]
@@ -158,11 +163,9 @@ def make_prediction(ts: pd.Series, model_path, h=6, full=False):
         return r_forecast
     else:
         return pd.Series(
-            np.array(r_forecast.rx2('y_hat')),
+            np.array(r_forecast.rx2("y_hat")),
             index=pd.date_range(
-                start=ts_end + pd.to_timedelta(ts_freq),
-                freq=ts_freq,
-                periods=h
-            )
+                start=ts_end + pd.to_timedelta(ts_freq), freq=ts_freq, periods=h
+            ),
         )
     return r_forecast
