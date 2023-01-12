0f3e7440fc6170cdc514a14dd8f2944cef669128
martins0n <33594071+martins0n@users.noreply.github.com>
2023-01-13 00:06:25 +0300

    examples-python-to-cython

---
+++ python-to-c/__main__.py
@@ -0,0 +1,4 @@
+import scalar
+
+if __name__ == "__main__":
+    print(scalar.scalar_prod([1, 2, 3], [4, 5, 6]))
---
+++ python-to-c/pysrc/scalar.py
@@ -0,0 +1,3 @@
+def scalar_prod(a: list, b: list):
+    return sum([i * j for i, j in zip(a, b)])
+
