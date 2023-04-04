c9ce10d8974a3ef54584de3b9a8b6be5d01c03fc
martins0n <33594071+martins0n@users.noreply.github.com>
2023-04-04 22:52:13 +0300

    fix: script

--- psql-copy/data_gen.py
+++ psql-copy/data_gen.py
@@ -5,6 +5,9 @@ import numpy as np
 
 segments = int(os.environ.get('SEGMENTS', 10000))
 n_timestamps = int(os.environ.get('TIMESTAMPS', 1000))
+
+format_ = str(os.environ.get('FORMAT', 'csv'))
+
 timestamp = pd.date_range('2019-01-01', periods=n_timestamps, freq='D')
 dataset = {
     "timestamp":[],
@@ -17,4 +20,10 @@ for i in range(segments):
     dataset["target"] += list(100 * np.random.normal(0, 1, n_timestamps))
     
 df = pd.DataFrame(dataset)
-df.to_csv(f"data_{segments}.csv", index=False, header=False)
+
+if format_ == "csv":
+    df.to_csv(f"data_{segments}.csv", index=False, header=False)
+elif format_ == "parquet":
+    df.to_parquet(f"data_{segments}.parquet", index=False, partition_cols=["segment"], compression="gzip")
+else:
+    raise ValueError("Format must be csv or parquet")
