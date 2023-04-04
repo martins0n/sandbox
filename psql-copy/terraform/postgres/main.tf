1e783bec6d6bd2b7da52e24c2b9032554c4a69e4
martins0n <33594071+martins0n@users.noreply.github.com>
2023-04-05 01:07:25 +0300

    fix: terraform yandex postgres start

---
+++ psql-copy/terraform/postgres/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+resource "yandex_mdb_postgresql_cluster" "mypg" {
+  name                = "mypg"
+  environment         = "PRESTABLE"
+  network_id          = yandex_vpc_network.mynet.id
+  security_group_ids  = [ yandex_vpc_security_group.pgsql-sg.id ]
+  deletion_protection = true
+
+  config {
+    version = 15
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = "network-ssd"
+      disk_size          = "20"
+    }
+  }
+
+  host {
+    zone      = "ru-central1-a"
+    name      = "mypg-host-a"
+    subnet_id = yandex_vpc_subnet.mysubnet.id
+  }
+}
+
+resource "yandex_mdb_postgresql_database" "db1" {
+  cluster_id = yandex_mdb_postgresql_cluster.mypg.id
+  name       = "db1"
+  owner      = var.db_username
+}
+
+resource "yandex_mdb_postgresql_user" "user1" {
+  cluster_id = yandex_mdb_postgresql_cluster.mypg.id
+  name       = var.db_username
+  password   = var.db_password
+}
+
+resource "yandex_vpc_network" "mynet" {
+  name = "mynet"
+}
+
+resource "yandex_vpc_subnet" "mysubnet" {
+  name           = "mysubnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.mynet.id
+  v4_cidr_blocks = ["10.5.0.0/24"]
+}
+
+resource "yandex_vpc_security_group" "pgsql-sg" {
+  name       = "pgsql-sg"
+  network_id = yandex_vpc_network.mynet.id
+
+  ingress {
+    description    = "PostgreSQL"
+    port           = 6432
+    protocol       = "TCP"
+    v4_cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
+
+variable "db_username" {
+  description = "The username for the DB master user"
+  type        = string
+}
+variable "db_password" {
+  description = "The password for the DB master user"
+  type        = string
+}
