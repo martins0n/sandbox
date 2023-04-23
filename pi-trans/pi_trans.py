40c46094e37445f86da0510c45366e6ef17e80ba
martins0n <33594071+martins0n@users.noreply.github.com>
2023-04-24 00:22:59 +0300

    fix: remembrance

--- pi-trans/pi_trans.py
+++ pi-trans/pi_trans.py
@@ -4,12 +4,16 @@ import torch.nn as nn
 import torch.functional as F
 import math
 
+
 class RoFormerSelfAttention(nn.Module):
     # https://github.com/huggingface/transformers/blob/2c8b508ccabea6638aa463a137852ff3b64be036/src/transformers/models/roformer/modeling_roformer.py#L209
     def __init__(
-        self, num_attention_heads: int, hidden_size: int,
-        rotary_value: bool = False, attention_probs_dropout_prob: float = 0,
-        is_decoder: bool = True
+        self,
+        num_attention_heads: int,
+        hidden_size: int,
+        rotary_value: bool = False,
+        attention_probs_dropout_prob: float = 0,
+        is_decoder: bool = True,
     ):
         super().__init__()
 
@@ -27,7 +31,10 @@ class RoFormerSelfAttention(nn.Module):
         self.rotary_value = rotary_value
 
     def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        new_x_shape = x.size()[:-1] + (
+            self.num_attention_heads,
+            self.attention_head_size,
+        )
         x = x.view(*new_x_shape)
         return x.permute(0, 2, 1, 3)
 
@@ -68,7 +75,11 @@ class RoFormerSelfAttention(nn.Module):
             value_layer = self.transpose_for_scores(self.value(hidden_states))
             if sinusoidal_pos is not None:
                 if self.rotary_value:
-                    query_layer, key_layer, value_layer = self.apply_rotary_position_embeddings(
+                    (
+                        query_layer,
+                        key_layer,
+                        value_layer,
+                    ) = self.apply_rotary_position_embeddings(
                         sinusoidal_pos, query_layer, key_layer, value_layer
                     )
                 else:
@@ -110,14 +121,18 @@ class RoFormerSelfAttention(nn.Module):
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
         context_layer = context_layer.view(*new_context_layer_shape)
 
-        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
+        outputs = (
+            (context_layer, attention_probs) if output_attentions else (context_layer,)
+        )
 
         if self.is_decoder:
             outputs = outputs + (past_key_value,)
         return outputs
 
     @staticmethod
-    def apply_rotary_position_embeddings(sinusoidal_pos, query_layer, key_layer, value_layer=None):
+    def apply_rotary_position_embeddings(
+        sinusoidal_pos, query_layer, key_layer, value_layer=None
+    ):
         # https://kexue.fm/archives/8265
         # sin [batch_size, num_heads, sequence_length, embed_size_per_head//2]
         # cos [batch_size, num_heads, sequence_length, embed_size_per_head//2]
@@ -127,28 +142,32 @@ class RoFormerSelfAttention(nn.Module):
         # cos [θ0,θ1,θ2......θd/2-1] -> cos_pos [θ0,θ0,θ1,θ1,θ2,θ2......θd/2-1,θd/2-1]
         cos_pos = torch.stack([cos, cos], dim=-1).reshape_as(sinusoidal_pos)
         # rotate_half_query_layer [-q1,q0,-q3,q2......,-qd-1,qd-2]
-        rotate_half_query_layer = torch.stack([-query_layer[..., 1::2], query_layer[..., ::2]], dim=-1).reshape_as(
-            query_layer
-        )
+        rotate_half_query_layer = torch.stack(
+            [-query_layer[..., 1::2], query_layer[..., ::2]], dim=-1
+        ).reshape_as(query_layer)
         query_layer = query_layer * cos_pos + rotate_half_query_layer * sin_pos
         # rotate_half_key_layer [-k1,k0,-k3,k2......,-kd-1,kd-2]
-        rotate_half_key_layer = torch.stack([-key_layer[..., 1::2], key_layer[..., ::2]], dim=-1).reshape_as(key_layer)
+        rotate_half_key_layer = torch.stack(
+            [-key_layer[..., 1::2], key_layer[..., ::2]], dim=-1
+        ).reshape_as(key_layer)
         key_layer = key_layer * cos_pos + rotate_half_key_layer * sin_pos
         if value_layer is not None:
             # rotate_half_value_layer [-v1,v0,-v3,v2......,-vd-1,vd-2]
-            rotate_half_value_layer = torch.stack([-value_layer[..., 1::2], value_layer[..., ::2]], dim=-1).reshape_as(
-                value_layer
-            )
+            rotate_half_value_layer = torch.stack(
+                [-value_layer[..., 1::2], value_layer[..., ::2]], dim=-1
+            ).reshape_as(value_layer)
             value_layer = value_layer * cos_pos + rotate_half_value_layer * sin_pos
             return query_layer, key_layer, value_layer
         return query_layer, key_layer
 
 
 class PITransformerBlock(nn.Module):
-    def __init__(self, d_model: int = 512, d_ff: int = 2048, num_heads: int = 4) -> None:
+    def __init__(
+        self, d_model: int = 512, d_ff: int = 2048, num_heads: int = 4
+    ) -> None:
         super().__init__()
         self.alpha = nn.Parameter(data=torch.randn(1))
-        self.num_heads =num_heads
+        self.num_heads = num_heads
         self.self_attn = RoFormerSelfAttention(
             hidden_size=d_model,
             num_attention_heads=num_heads,
@@ -156,21 +175,24 @@ class PITransformerBlock(nn.Module):
         self.ff = nn.Sequential(
             nn.Linear(in_features=d_model, out_features=d_ff),
             nn.ReLU(),
-            nn.Linear(in_features=d_ff, out_features=d_model)
+            nn.Linear(in_features=d_ff, out_features=d_model),
         )
-    
+
     def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
         mask = mask_gen(x, self.num_heads)
         x = x + self.alpha * self.self_attn(hidden_states=x, attention_mask=mask)[0]
         x = x + self.alpha * self.ff(x)
         return x
-    
+
+
 class LogMeanScaler(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.register_buffer("mu", None)
-        
-    def forward(self, x: torch.Tensor, shift: int, window_size: int, inverse: bool = False) -> torch.Tensor:
+
+    def forward(
+        self, x: torch.Tensor, shift: int, window_size: int, inverse: bool = False
+    ) -> torch.Tensor:
         if shift == 0:
             mean_slice = slice(-window_size, None)
         else:
@@ -184,31 +206,46 @@ class LogMeanScaler(nn.Module):
 
 
 class PITransformer(nn.Module):
-    def __init__(self, d_model: int = 512, d_ff: int = 2048, num_heads: int =  4, n_layers: int = 4) -> None:
+    def __init__(
+        self,
+        d_model: int = 512,
+        d_ff: int = 2048,
+        num_heads: int = 4,
+        n_layers: int = 4,
+        scale: bool = True,
+    ) -> None:
         super().__init__()
         self.n_layers = n_layers
-        self.scaler = LogMeanScaler()
+        if scale:
+            self.scaler = LogMeanScaler()
+        else:
+            def identity(x, *args, **kwargs):
+                return x
+            self.scaler = identity
         self.transformer = nn.Sequential(
             nn.Linear(in_features=1, out_features=d_model),
-            *[PITransformerBlock(d_model=d_model, d_ff=d_ff, num_heads=num_heads) for _ in range(n_layers)],
+            *[
+                PITransformerBlock(d_model=d_model, d_ff=d_ff, num_heads=num_heads)
+                for _ in range(n_layers)
+            ],
             nn.Linear(in_features=d_model, out_features=1)
         )
-        
-    def forward(self, x: torch.Tensor, shift: int, window_size: int, mask=None) -> torch.Tensor:
+
+    def forward(
+        self, x: torch.Tensor, shift: int, window_size: int, mask=None
+    ) -> torch.Tensor:
         x = self.scaler(x, inverse=False, shift=shift, window_size=window_size)
         x = self.transformer(x)
         x = self.scaler(x, inverse=True, shift=shift, window_size=window_size)
         return x
 
+
 def mask_gen(x, n_heads):
-    mask = torch.tril(
-        torch.ones((x.shape[1], x.shape[1])),
-        diagonal=0
-    )
-    
-    mask[mask == 0] = float('-inf')
+    mask = torch.tril(torch.ones((x.shape[1], x.shape[1])), diagonal=0)
+
+    mask[mask == 0] = float("-inf")
     mask[mask == 1] = 0.0
-    
+
     mask = mask.unsqueeze(0).unsqueeze(0).repeat(x.shape[0], n_heads, 1, 1)
     return mask
 
@@ -216,28 +253,24 @@ def mask_gen(x, n_heads):
 if __name__ == "__main__":
     x = torch.randn(2, 10, 512)
     n_heads = 4
-    mask = torch.tril(
-        torch.ones((10, 10)),
-        diagonal=0
-    )
-    
-    mask[mask == 0] = float('-inf')
+    mask = torch.tril(torch.ones((10, 10)), diagonal=0)
+
+    mask[mask == 0] = float("-inf")
     mask[mask == 1] = 0.0
-    
+
     mask = mask.unsqueeze(0).unsqueeze(0).repeat(x.shape[0], n_heads, 1, 1)
-    
+
     print(mask)
     print(mask.shape)
     model = PITransformerBlock(num_heads=n_heads)
     y = model(x, mask)
     print(y)
     print(y.shape)
-    
-    
+
     x = torch.randn(2, 10, 1) + 100
-    
+
     model = PITransformer(horizon=10)
-    
+
     y = model(x, shift=2, window_size=2)
-    
+
     print(y)
--- pi-trans/train.py
+++ pi-trans/train.py
@@ -1,4 +1,3 @@
-from imp import load_source
 from etna.analysis import plot_backtest
 from etna.models.base import DeepBaseNet, DeepBaseModel
 from etna.models.nn.rnn import RNNNet
@@ -30,48 +29,73 @@ from etna.pipeline import Pipeline
 from etna.transforms import LagTransform, StandardScalerTransform
 from etna.datasets import generate_periodic_df, generate_from_patterns_df
 
+
 def set_seed(seed: int):
     random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
 
+
 from typing import Iterator, Optional, Dict, Any
+
 
 class MASELoss(nn.Module):
     def __init__(self, seasonality: int = 7):
         super().__init__()
         self.seasonality = seasonality
-    
+
     def forward(self, input, target, in_sample):
         sample_mean = (target - input).abs().mean(dim=1)
-        seasonal_sample_mean = (in_sample[:, self.seasonality:] - in_sample[:, :-self.seasonality]).abs().mean(dim=1)
+        seasonal_sample_mean = (
+            (in_sample[:, self.seasonality :] - in_sample[:, : -self.seasonality])
+            .abs()
+            .mean(dim=1)
+        )
         return (sample_mean / (seasonal_sample_mean + 1e-2)).mean()
-        
-
 
 
 class PITransformerNet(RNNNet, DeepBaseNet):
-    def __init__(self, lr, loss, d_model: int = 512, d_ff: int = 2048, num_heads: int =  4, n_layers: int = 4, optimizer_params = None):
+    def __init__(
+        self,
+        lr,
+        loss,
+        d_model: int = 512,
+        d_ff: int = 2048,
+        num_heads: int = 4,
+        n_layers: int = 4,
+        scale: bool = True,
+        optimizer_params=None,
+    ):
         super(DeepBaseNet, self).__init__()
-        self.pi_trans = PITransformer(d_model=d_model, d_ff=d_ff, num_heads = num_heads, n_layers=n_layers)
+        self.pi_trans = PITransformer(
+            d_model=d_model, d_ff=d_ff, num_heads=num_heads, n_layers=n_layers, scale=scale
+        )
         self.num_heads = num_heads
         self.lr = lr
         self.loss = loss
         self.optimizer_params = optimizer_params
 
     def forward(self, x):
-        encoder_real = x["encoder_real"].float()  # (batch_size, encoder_length-1, input_size)
+        encoder_real = x[
+            "encoder_real"
+        ].float()  # (batch_size, encoder_length-1, input_size)
         decoder_target = x["decoder_target"].float()  # (batch_size, decoder_length, 1)
         decoder_real = x["decoder_real"].float()
         decoder_length = decoder_target.shape[1]
         forecast = torch.zeros_like(decoder_target)  # (batch_size, decoder_length, 1)
         mask = mask_gen(encoder_real, self.num_heads)
-        input_ = torch.cat([encoder_real, decoder_real[:, :1]], dim=1)  
-        forecast[:, 0, :] = self.pi_trans(input_, shift=0, window_size=decoder_length, mask=mask)[:, -1 , :]
+        input_ = torch.cat([encoder_real, decoder_real[:, :1]], dim=1)
+        forecast[:, 0, :] = self.pi_trans(
+            input_, shift=0, window_size=decoder_length, mask=mask
+        )[:, -1, :]
         for t in range(1, decoder_length):
-            input_ = torch.cat([encoder_real, decoder_real[:, :1], forecast[:, :t, :]], dim=1)
+            input_ = torch.cat(
+                [encoder_real, decoder_real[:, :1], forecast[:, :t, :]], dim=1
+            )
             mask = mask_gen(input_, self.num_heads)
-            forecast[:, t, :] = self.pi_trans(input_, shift=t, window_size=decoder_length, mask=mask)[:, -1 , :]
+            forecast[:, t, :] = self.pi_trans(
+                input_, shift=t, window_size=decoder_length, mask=mask
+            )[:, -1, :]
         return forecast
 
     def step(self, batch, *args, **kwargs):
@@ -80,11 +104,17 @@ class PITransformerNet(RNNNet, DeepBaseNet):
         decoder_target = batch["decoder_target"].float()
         encoder_target = batch["encoder_target"].float()
         input_ = torch.cat([encoder_real, decoder_real], dim=1)
-        mask = mask_gen(input_, self.num_heads) 
-        output = self.pi_trans(input_, shift=decoder_real.shape[1], window_size=decoder_real.shape[1], mask=mask)[:, -decoder_target.shape[1]:, :]
-        #loss = self.loss(output, decoder_target, encoder_target)
+        mask = mask_gen(input_, self.num_heads)
+        output = self.pi_trans(
+            input_,
+            shift=decoder_real.shape[1],
+            window_size=decoder_real.shape[1],
+            mask=mask,
+        )[:, -decoder_target.shape[1] :, :]
+        # loss = self.loss(output, decoder_target, encoder_target)
         loss = self.loss(output, decoder_target)
         return loss, decoder_target, output
+
     def training_step(self, batch: dict, *args, **kwargs):  # type: ignore
         """Training step.
 
@@ -101,15 +131,17 @@ class PITransformerNet(RNNNet, DeepBaseNet):
         loss, a, b = self.step(batch, *args, **kwargs)  # type: ignore
         self.log("train_loss", loss, on_epoch=True)
         return loss
-class PITransformerModel(DeepBaseModel):
 
+
+class PITransformerModel(DeepBaseModel):
     def __init__(
         self,
         decoder_length: int,
         encoder_length: int,
         d_model: int = 512,
         d_ff: int = 2048,
-        num_heads: int =  4,
+        scale: bool = True,
+        num_heads: int = 4,
         n_layers: int = 4,
         lr: float = 1e-3,
         loss: Optional["torch.nn.Module"] = None,
@@ -130,7 +162,10 @@ class PITransformerModel(DeepBaseModel):
                 n_layers=n_layers,
                 lr=lr,
                 loss=nn.MSELoss() if loss is None else loss,
-                optimizer_params= dict() if optimizer_params is None else optimizer_params,
+                scale=scale,
+                optimizer_params=dict()
+                if optimizer_params is None
+                else optimizer_params,
             ),
             decoder_length=decoder_length,
             encoder_length=encoder_length,
@@ -143,6 +178,7 @@ class PITransformerModel(DeepBaseModel):
             split_params=split_params,
         )
 
+
 def train_backtest(
     horizon: int = 7,
     n_epochs: int = 100,
@@ -150,38 +186,50 @@ def train_backtest(
     batch_size: int = 64,
     seed: int = 11,
     dataset_path: str = "/Users/marti/Projects/etna/examples/data/example_dataset.csv",
-    experiments_folder: pathlib.Path = pathlib.Path("experiments"),
     dataset_freq: str = "D",
     d_model: int = 512,
     d_ff: int = 1024,
     n_layers: int = 4,
     num_heads: int = 4,
     patience: int = 10,
+    scale: bool = True,
 ):
     parameters = dict(locals())
     parameters["dataset_path"] = str(dataset_path)
-    parameters["experiments_folder"] = str(experiments_folder)
 
     set_seed(seed)
 
-    original_df = pd.read_parquet(dataset_path)
-    #original_df = generate_from_patterns_df(
-    #    periods=600, start_time="2020-01-01", patterns=[[30, 50, 50, 100, 100]], freq='D', add_noise=True,  sigma=1,
-    #)
+    # check if csv or parquet
+    if dataset_path.endswith(".csv"):
+        original_df = pd.read_csv(dataset_path)
+    elif dataset_path.endswith(".parquet"):
+        original_df = pd.read_parquet(dataset_path)
+    elif dataset_path == "pattern":
+        original_df = generate_from_patterns_df(
+            periods=300, start_time="2019-01-01", 
+            patterns=[[200, 300, 200, 300, 20], [100, 200, 100, 200, 10]],
+            add_noise=True,
+        )
+    else:
+        raise ValueError("Unknown dataset format")
+
     df = TSDataset.to_dataset(original_df)
     ts = TSDataset(df, freq=dataset_freq)
 
-
     model = PITransformerModel(
         n_layers=n_layers,
-        #loss = MASELoss(1),
-        loss = nn.L1Loss(),
+        # loss = MASELoss(1),
+        loss=nn.L1Loss(),
         d_model=d_model,
+        scale=scale,
         num_heads=num_heads,
         d_ff=d_ff,
         decoder_length=horizon,
-        encoder_length=2 * horizon + 1,
-        trainer_params={"max_epochs": n_epochs, "callbacks": [EarlyStopping(monitor="val_loss", patience=patience)]},
+        encoder_length=3 * horizon + 1,
+        trainer_params={
+            "max_epochs": n_epochs,
+            "callbacks": [EarlyStopping(monitor="val_loss", patience=patience)],
+        },
         lr=lr,
         train_batch_size=batch_size,
         split_params={"train_size": 0.8},
@@ -190,6 +238,9 @@ def train_backtest(
     pipeline = Pipeline(
         model=model,
         horizon=horizon,
+        transforms=[
+            StandardScalerTransform(in_column="target", mode="per-segment"),
+        ],
     )
 
     tslogger.add(
@@ -200,13 +251,15 @@ def train_backtest(
     )
 
     metrics = [SMAPE(), MSE(), MAE(), Sign()]
-    metrics, forecast, fold_info = pipeline.backtest(ts, metrics=metrics, n_folds=1, n_jobs=1)    
+    metrics, forecast, fold_info = pipeline.backtest(
+        ts, metrics=metrics, n_folds=1, n_jobs=1
+    )
     print(metrics)
     print(forecast.tail(10))
     print(ts.df.tail(10))
     # plot_backtest(forecast, ts, history_len=20)
     # plt.show(block=True)
-    
+
 
 if __name__ == "__main__":
     typer.run(train_backtest)
